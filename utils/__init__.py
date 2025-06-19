9c71cea82943047a69f3c7bfd13dec22b1ef7040
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-06-19 21:42:28 +0530

    change config

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .config import llm, fast_llm, AgentState, embeddings, generate_prompt_template, agent_prompt_template
+from .config import llm, fast_llm, AgentState, embeddings
 from .vector_store import VectorStore
 from .loaders import Loader
 from .translate import translate_text, detect
