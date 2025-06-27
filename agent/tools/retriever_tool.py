92e6a3621c554ec26ea2bfe9243b62a5c3c4f3d4
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-06-27 12:28:23 +0530

    add cache for retriever tool

--- agent/tools/retriever_tool.py
+++ agent/tools/retriever_tool.py
@@ -1,5 +1,26 @@
 from langchain.tools.retriever import create_retriever_tool
 from utils import VectorStore
+from functools import lru_cache
+
+
+
+def get_cached_retriever_tool(web_name:str):
+    """
+        Returns a cached retriever tool for the given web_name.
+
+        This is a caching layer on top of the `get_retriever_tool` function, which
+        creates a retriever tool for the given web_name. The cache is memoized
+        using the `lru_cache` decorator.
+
+        Args:
+            web_name(str): The name of the website for which to get the cached
+                retriever tool.
+
+        Returns:
+            A cached retriever tool linked to the vector store for the given
+            web_name.
+    """
+    return VectorStore(web_name=web_name).get_vector_store()
 
 
 def get_retriever_tool(web_name:str):
@@ -16,7 +37,7 @@ def get_retriever_tool(web_name:str):
     """
 
     retriever_tool = create_retriever_tool(
-        VectorStore(web_name=web_name).get_vector_store(),
+        retriever=get_cached_retriever_tool(web_name=web_name),
         name=f"{web_name}_website_knowledge_search",
         description=f"Search {web_name} services, pricing, processes, or product details from structured website data."
     )
