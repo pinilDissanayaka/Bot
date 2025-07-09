b8aed6589a909f37dedf648a27658e77e7ac9ae4
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-07-09 20:56:10 +0530

    remove eailing

--- agent/tools/email.py
+++ agent/tools/email.py
@@ -22,21 +22,6 @@ def contact(name: str, senders_email: str, phone_number: str, message_body: str)
     Returns a string with a success message or an error message if there was an exception.
     """
     try:
-        smtp_host = os.getenv("HOST")
-        smtp_port = 587
-        smtp_user = "api"
-        smtp_pass = os.getenv("EMAIL_API_KEY")
-        sender_email = os.getenv("SENDER")
-        receiver_email = os.getenv("RECEIVER")
-
-        if not sender_email or not receiver_email:
-            raise ValueError("Missing SENDER or RECEIVER in environment variables")
-
-        msg = MIMEText(f"Message from: {senders_email}\n\n{message_body}")
-        msg["Subject"] = f"Contact from {senders_email}"
-        msg["From"] = formataddr(("Private Person", sender_email))
-        msg["To"] = formataddr(("A Test User", receiver_email))
-        
         conn= psycopg2.connect(os.getenv("DATABASE_URL"))
         cursor = conn.cursor()
         # Insert contact details into the database
@@ -47,11 +32,6 @@ def contact(name: str, senders_email: str, phone_number: str, message_body: str)
         conn.commit()
         cursor.close()
         conn.close()
-        # Send email
-        with smtplib.SMTP(smtp_host, smtp_port) as server:
-            server.starttls()
-            server.login(smtp_user, smtp_pass)
-            server.send_message(msg)
 
         return "Email sent successfully!, our team will get back to you soon."
 
